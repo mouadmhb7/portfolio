89d37b0cb0f049c4c7dfd823867190e9c928df68
mouad <mouadmhb6@gmail.com>
2024-12-28 14:33:56 +0100

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import  styles from'./App.module.css'
 import  Navbar  from "./Components/Navbar.jsx"
 import { Hero } from './Components/Hero/Hero.jsx'
-import About from './Components/About/About.jsx'
+import About from './Components/MyAbout/About.jsx'
 import Projects from './Components/Projects/Projects.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 function App() {
